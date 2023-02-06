b1dcee00075dcd59abd2d34ada5d05e9dacfb8a5
andrianiavotahina <123719137+andrianiavotahina@users.noreply.github.com>
2023-02-06 20:57:18 +0100

    modification

--- src/Configuration.js
+++ src/Configuration.js
@@ -1,2 +1,3 @@
 //export const url="http://172.31.0.1:8080/encher"
-export const url="https://webservice20-production.up.railway.app/encher"
+//export const url="https://webservice20-production.up.railway.app/encher"
+export const url="https://webservice20-production-264c.up.railway.app/encher"
